480762b49f488eb96db48e25c57ff4979d43bacb
AliAhmad44550 <aliahmad44550@gmail.com>
2024-02-19 16:46:37 +0500

    add visibility

--- android/src/main/kotlin/dev/duynp/flutter_health_connect/FlutterHealthConnectPlugin.kt
+++ android/src/main/kotlin/dev/duynp/flutter_health_connect/FlutterHealthConnectPlugin.kt
@@ -54,9 +54,8 @@ class FlutterHealthConnectPlugin(private var channel: MethodChannel? = null) : F
         context = flutterPluginBinding.applicationContext
         client = HealthConnectClient.getOrCreate(context!!)
         replyMapper.registerModule(JavaTimeModule())
-        replyMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-//     replyMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
-        replyMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        replyMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+         replyMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
